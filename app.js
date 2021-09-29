188d1dbe2ca7d06a887e503e432f03125ef65875
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2021-09-29 21:00:22 +0530

    added translator

--- app.js
+++ app.js
@@ -2,9 +2,31 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with the server! Try again later");
+}
+
 function clickHandler(){
-    outputDiv.innerText = "dfndfjndfhjndfj " + txtInput.value;
+    //taking input
+    var inputText = txtInput.value;
+
+    //calling srver for processing and pring output on screen
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => { 
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click",  clickHandler)
-
